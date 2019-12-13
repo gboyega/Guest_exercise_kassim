dc91019631c9f5d2fff0677a6975e1bffbd03a28
gboyega <jaiyeolagboyega@gmail.com>
2019-12-13 16:26:00 +0100

    file upload to S3

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
-const express = require("express");
+const fs = require('fs');
+const AWS = require('aws-sdk');
+require("dotenv").config();
 
-const app = express();
-const bodyParser = require("body-parser");
 
-const port = process.env.PORT || 4005;
+const ID = process.env.ID;
+const SECRET = process.env.SECRET;
+const BUCKET_NAME = process.env.BUCKET_NAME;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-const routes = require("./routes/userRoutes");
-routes(app);
-
-app.listen(port, () => {
-  console.log(`server started on port ${port}`);
+const s3 = new AWS.S3({
+  accessKeyId: ID,
+  secretAccessKey: SECRET
 });
+
+console.log(s3);
+const uploadFile = fileName => {
+  const fileContent = fs.readFileSync(fileName);
+  
+  const params = {
+    Bucket: BUCKET_NAME,
+    Key: fileName,
+    Body: fileContent
+  };
+  
+  s3.upload(params, function(err, data) {
+    if (err) {
+      throw err;
+    }
+    console.log(`File uploaded successfully. ${data.Location}`);
+  });
+};
+
+uploadFile("images.png");
